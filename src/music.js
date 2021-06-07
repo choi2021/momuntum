7f14ff35a9698fa8b05afcf323bfb01d201238d2
unknown <80830981+choi2021@users.noreply.github.com>
2021-06-08 03:20:02 +0900

    music added

--- src/music.js
+++ src/music.js
@@ -1,2 +1,27 @@
+const theme = new Audio(`music/Mia  Sebastians Theme  La La Land OST.mp3`);
+const musicBtn = document.querySelector(".header__music__btn");
 
+let started = false;
+
+musicBtn.addEventListener("click", onClickMusicBtn);
+
+function playMusic(song) {
+    song.play()
+}
+
+function pauseMusic(song) {
+    song.pause();
+}
+
+function onClickMusicBtn() {
+    if (!started) {
+        musicBtn.innerHTML = `<i class="fas fa-stop"></i>`;
+        playMusic(theme);
+        started = true;
+    } else {
+        musicBtn.innerHTML=`<i class="fas fa-play">`;
+        pauseMusic(theme);
+        started = false;
+    }
+}
     
